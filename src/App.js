674f57bd5d335687021f9c854a4359e5615f3ea6
Arshad. S <arshad1@gmail.com>
2021-12-13 21:34:07 +0530

    Removed box tags and added containers for holding cards for player and dealer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import './App.css';
 import GameMenu from "./BlackjackGame/Menu";
-import { MDBContainer } from "mdbreact";
+
 import React, { useState, useEffect } from 'react';
 import PlayButtons from './BlackjackGame/PlayButtons';
 import { GameBoard } from './BlackjackGame/GameBoard';
+import { Deck} from './BlackjackGame/CardData';
 function App() {
 const [PlayButtonShow,setPlayButtonShow] = useState(true);
-const [QuitButtonShow,setQuitButtonShow] = useState(false);
+  const [QuitButtonShow, setQuitButtonShow] = useState(false);
+  const [DeckShuffleld, setDeckShuffeled] = useState([]);
+  const [DealerDeck, setDealerDeck] = useState([]);
+  const [PlayerDeck, serPlayerDeck] = useState([]);
+  useEffect(() => {
+    console.log("shuffleStart");
+    shuffleDeck(setDeckShuffeled);
+console.log("shuffleEnd");
+  }, () => { });
+
   return (
     <div className="App">
-      <MDBContainer>
         <GameMenu PlayButtonShow={PlayButtonShow} QuitButtonShow={QuitButtonShow} setPlayButtonShow={setPlayButtonShow} setQuitButtonShow={setQuitButtonShow }/>
           <GameBoard  />
-          <PlayButtons/>
-      </MDBContainer>
+      <PlayButtons />
+    
     </div>
   );
+}
+const shuffleDeck = (fn) => {
+  let tempDeck = Deck.map(a => a);
+  for (let i = 0; i < 100; i++){
+    let rand1= Math.floor (Math.random() * 100 % 56);
+    let rand2 = Math.floor(Math.random() * 100 % 56);
+    let tempCard = tempDeck[rand1];
+    tempDeck[rand1] = tempDeck[rand2];
+    tempDeck[rand2] = tempCard;
+  }
+  fn(tempDeck);
 }
 
 export default App;
