c5f261fe1a7697558f4c47e389bbbf941001c57d
Arshad. S <arshad1@gmail.com>
2023-09-24 21:21:11 +0530

    I have no idea what I am doing with the card UI, restarting on card UI

--- src/BlackjackGame/GameBoard.js
+++ src/BlackjackGame/GameBoard.js
@@ -6,7 +6,12 @@ export const GameBoard = () => {
     return (
     <div >
         <div >Dealer</div>
-        <div className='game-deck'><div className='card-back'>card</div></div>
+        <div className='game-deck'>
+          <div className='card-back'>
+          </div>card
+          <div className='card-back'>
+        </div>
+        </div>
         <div>Player</div>
       
       </div>
